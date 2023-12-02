20e528c893e3acb6c70aa11403f5f81f4371a4f4
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-12-03 00:41:45 +0100

    first commit

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,119 @@
+package com.example;
+
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.resourcemanager.compute.models.Disk;
+import com.azure.resourcemanager.compute.models.KnownLinuxVirtualMachineImage;
+import com.azure.resourcemanager.compute.models.KnownWindowsVirtualMachineImage;
+import com.azure.resourcemanager.compute.models.VirtualMachine;
+import com.azure.resourcemanager.compute.models.VirtualMachineSizeTypes;
+import com.azure.resourcemanager.network.models.Network;
+import com.azure.core.management.Region;
+import com.azure.resourcemanager.resources.fluentcore.model.Creatable;
+import com.azure.core.management.profile.AzureProfile;
+
+import java.util.Date;
+
+/**
+ * Azure Compute sample for managing virtual machines -
+ *  - Create a virtual machine with managed OS Disk
+ *  - Start a virtual machine
+ *  - Stop a virtual machine
+ *  - Restart a virtual machine
+ *  - Update a virtual machine
+ *    - Tag a virtual machine (there are many possible variations here)
+ *    - Attach data disks
+ *    - Detach data disks
+ *  - List virtual machines
+ *  - Delete a virtual machine.
+ */
+
+
+public class Main {
+
+    public static String randomResourceName(AzureResourceManager azure, String prefix, int maxLen) {
+        return azure.resourceGroups().manager().internalContext().randomResourceName(prefix, maxLen);
+    }
+
+    public static void main(String[] args) {
+    
+        //=============================================================
+        // 1. Azure SDK for Java: Azure Authentication
+        //=============================================================
+
+        AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
+        TokenCredential credential = new DefaultAzureCredentialBuilder()
+            .authorityHost(profile.getEnvironment().getActiveDirectoryEndpoint())
+            .build();
+        AzureResourceManager azureResourceManager = AzureResourceManager
+            .authenticate(credential, profile)
+            .withDefaultSubscription();
+
+        //=============================================================
+        // 2. Azure SDK for Java: Create a resource group
+        //=============================================================
+
+        final String rgName = "myFirstResourceGroup";
+        System.out.println("Creating a resource group with name: " + rgName);
+
+        azureResourceManager.resourceGroups().define(rgName)
+                .withRegion(Region.US_WEST)
+                .create();
+
+        //------------------------------------------------------------------------------------------------------------
+        // 3. Prepare a creatable data disk for VM
+        //------------------------------------------------------------------------------------------------------------
+
+        Creatable<Disk> dataDiskCreatable = azureResourceManager.disks().define(Main.randomResourceName(azureResourceManager, "dsk-", 15))
+                .withRegion(Region.US_WEST)
+                .withExistingResourceGroup(rgName)
+                .withData()
+                .withSizeInGB(100);
+
+        //------------------------------------------------------------------------------------------------------------
+        // 4. Create a data disk to attach to VM
+        //------------------------------------------------------------------------------------------------------------
+
+        Disk dataDisk = azureResourceManager.disks()
+                .define(Main.randomResourceName(azureResourceManager, "dsk-", 15))
+                    .withRegion(Region.US_WEST)
+                    .withNewResourceGroup(rgName)
+                    .withData()
+                    .withSizeInGB(50)
+                    .create();
+
+        System.out.println("Creating a Windows VM");
+
+        Date t1 = new Date();
+
+        //------------------------------------------------------------------------------------------------------------
+        // 5. Create a VM
+        //------------------------------------------------------------------------------------------------------------
+
+        String windowsVMName = "mynewvm19742000";
+        String userName = "azureuser";
+        String password = "Luiscoco123456";
+
+        VirtualMachine windowsVM = azureResourceManager.virtualMachines()
+                .define(windowsVMName)
+                    .withRegion(Region.US_WEST)
+                    .withNewResourceGroup(rgName)
+                    .withNewPrimaryNetwork("10.0.0.0/28")
+                    .withPrimaryPrivateIPAddressDynamic()
+                    .withoutPrimaryPublicIPAddress()
+                    .withPopularWindowsImage(KnownWindowsVirtualMachineImage.WINDOWS_SERVER_2019_DATACENTER_GEN2)
+                    .withAdminUsername(userName)
+                    .withAdminPassword(password)
+                    .withNewDataDisk(10)
+                    .withNewDataDisk(dataDiskCreatable)
+                    .withExistingDataDisk(dataDisk)
+                    .withSize(VirtualMachineSizeTypes.fromString("Standard_E2s_v3"))
+                    .create();
+
+        Date t2 = new Date();
+        System.out.println("Created VM: (took " + ((t2.getTime() - t1.getTime()) / 1000) + " seconds) " + windowsVM.id());
+    }
+}
